d6264dc831f083dab1dcd492a9f8bbed32dcc26e
ANUSHAVAVILTHOTA <anusha.v1999@gmail.com>
2019-03-27 14:22:51 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import SearchResultItem from './SearchResultItem';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello World
+        <SearchResultItem></SearchResultItem>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/SearchResultItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class SearchResultItem extends Component{
+    render() {
+        return (
+          <div>
+            <p>Anu</p>
+            <p>From E3 CSE</p>
+          </div>
+        );
+      }
+}
+
+export default SearchResultItem;
